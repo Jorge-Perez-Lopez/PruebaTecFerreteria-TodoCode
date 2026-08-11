b0ea6a8334bbfdd00087964ba03acb361428e5f4
Jorge Pérez López <perezlopezj07@hotmail.com>
2026-08-11 10:50:50 +0200

    Initial Commit

---
+++ src/main/java/com/perezjorge/PruebaTecFerreteria/PruebaTecFerreteriaApplication.java
@@ -0,0 +1,13 @@
+package com.perezjorge.PruebaTecFerreteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecFerreteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecFerreteriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perezjorge/PruebaTecFerreteria/PruebaTecFerreteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perezjorge.PruebaTecFerreteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecFerreteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
